84932fc408b7a6a65e056fce55d21296fb1452bb
Leah Anestos <leahkanestos@gmail.com>
2023-09-03 00:08:25 -0400

    making pipes show re render once screen hit the end of its cycle

--- physics.js
+++ physics.js
@@ -1,4 +1,11 @@
 import Matter from "matter-js";
+import { getPipeSizePosPair } from "./utils/randomLogic";
+import { Dimensions } from 'react-native';
+
+
+const windowHeight = Dimensions.get('window').height
+const windowWidth = Dimensions.get('window').width
+
 
 const Physics = (entities, {touches, time, dispatch}) => {
     let engine = entities.physics.engine
@@ -14,6 +21,13 @@ const Physics = (entities, {touches, time, dispatch}) => {
     Matter.Engine.update(engine, time.delta)
 
     for (let index = 1; index <= 2; index++ ) {
+
+        if(entities[`ObstacleTop${index}`].body.bounds.max.x <= 0 ){
+            const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
+
+            Matter.Body.setPosition(entities[`ObstacleTop${index}`].body, pipeSizePos.pipeTop.pos)
+            Matter.Body.setPosition(entities[`ObstacleBottom${index}`].body, pipeSizePos.pipeBottom.pos)
+        }
     
         Matter.Body.translate(entities[`ObstacleTop${index}`].body, {x: -3, y: 0})
         Matter.Body.translate(entities[`ObstacleBottom${index}`].body, {x: -3, y: 0})
