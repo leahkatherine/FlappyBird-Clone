7b98d48c9aae8541b98867c87bce6962779ef582
Leah Anestos <leahkanestos@gmail.com>
2023-09-02 21:24:06 -0400

    Bird.js, index.js, physics.js, created

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
+import Physics from './physics';
+import { useState } from 'react';
 
 export default function App() {
+  const [running, setRunning] = useState(false)
   return (
     <View style={{flex: 1}}>
       <GameEngine
+      systems={[Physics]}
+      entities={entities()}
+      running={running}
         style={{
           position: 'absolute', top: 0,left: 0, right: 0, bottom: 0 }}
       >
